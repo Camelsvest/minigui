713653e493026a34ed0ae0d3fc748b1ac5e4bae6
camelsvest <2954791313@qq.com>
2019-03-17 23:48:58 +0800

    add ./configure with its long parameters.

--- autogen.sh
+++ autogen.sh
@@ -5,3 +5,4 @@ aclocal
 autoheader
 automake --add-missing --foreign --copy
 autoconf
+./configure --disable-screensaver --enable-procs --enable-debug --enable-tracemsg --enable-ttfcache --enable-ctrliconview --enable-ctrlscrollview --enable-ctrltreeview --enable-ctrlcoolbar --enable-ctrlmonthcal --with-ft2-includes=/usr/local/include CFLAGS="-g -O0"
