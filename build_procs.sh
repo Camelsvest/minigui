666075c55bdf07dc1d7b6fa6c16e27e23bfa342f
camelsvest <2945791313@qq.com>
2019-07-01 18:14:08 +0800

    Add bash script build_procs.sh

---
+++ build_procs.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+
+#./configure --disable-screensaver --enable-procs --enable-debug --enable-tracemsg --enable-ttfcache --enable-ctrliconview --enable-ctrlscrollview --enable-ctrltreeview --enable-ctrlcoolbar --enable-ctrlmonthcal --with-ft2-includes=/usr/local/include CFLAGS="-g -O0"
+./configure --enable-procs --disable-screensaver --with-ft2-includes=/usr/local/include CFLAGS="-g -O0"
